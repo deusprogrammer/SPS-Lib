cffb898593a9bf35fa14cf01db5b491681b86c7f
Michael C. Main <mike.main@rackspace.com>
2013-11-04 09:11:33 -0600

    Initial commit

---
+++ src/udpbroadcast/UDPBroadcast.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udpbroadcast;
+
+/**
+ *
+ * @author Michael
+ */
+public class UDPBroadcast {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Client client = new Client();
+        client.run();
+    }
+}
